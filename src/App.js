9fa6bff9f5617eb0d0c45527c83b395f80dc5239
rabindra20 <rabindra2079@gmail.com>
2022-11-09 10:05:43 +0545

    change in app.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
-// function add(numbers) {
-//   var result = 0;
-//   var parts = numbers.split(',');
-//   for (var i = 0; i < parts.length; i++) {
-//       var integer = parseInt(parts[i]);
-//       if (!isNaN(integer)) {
-//           if (integer >= 0) {
-//               if (integer <= 1000) {
-//                   result += integer;
-//               }
-//           }
-//       }
-//   }
+function add(numbers) {
+  var result = 0;
+  var parts = numbers.split(',');
+  for (var i = 0; i < parts.length; i++) {
+      var integer = parseInt(parts[i]);
+      if (!isNaN(integer)) {
+          if (integer >= 0) {
+              if (integer <= 1000) {
+                  result += integer;
+              }
+          }
+      }
+  }
 
-//   return result;
-// }
+  return result;
+}
 
-// const greet = (firstName, lastName, greeting) => {
-//   if (typeof firstName === 'string') {
-//     if (typeof lastName === 'string') {
-//       if (typeof greeting === 'string') {
-//         return `${greeting}, ${firstName}${lastName}`;
-//       }
-//     }
-//   }
-// }
+const greet = (firstName, lastName, greeting) => {
+  if (typeof firstName === 'string') {
+    if (typeof lastName === 'string') {
+      if (typeof greeting === 'string') {
+        return `${greeting}, ${firstName}${lastName}`;
+      }
+    }
+  }
+}
 function App() {
   return (
     <div className="App">
