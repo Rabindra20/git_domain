44ef684ecc2227c355653d93193e26aeb1e44762
rabindra20 <rabindra2079@gmail.com>
2022-12-29 13:22:18 +0545

    updated app.js

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> HI.
+          Edit <code>src/App.js</code> hello.
         </p>
         <a
           className="App-link"
