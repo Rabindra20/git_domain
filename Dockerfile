b5b5becd9010a459e1674ed8708a00a20e1a4388
Rabindra Raj Sah <53372486+Rabindra20@users.noreply.github.com>
2023-03-02 17:12:49 +0545

    ddd (#23)
    * updated workflow
    
    * updated workflow
    
    * update sonarqube
    
    * update sonarqube
    
    * added json
    
    * updated sonar properties
    
    * updated sonar properties
    
    * updated sonar properties
    
    * updated workflow
    
    * updated workflow
    
    * updated workflow
    
    * updated workflow
    
    * updated workflow
    
    * change in app.js for test
    
    * change in app.js for test
    
    * change in app.js for test
    
    * change in app.js for test
    
    * change in app.js for test
    
    * change in app.js
    
    * change in app.js
    
    * change in app.js
    
    * change in app.js
    
    * updated docker
    
    * updated docker
    
    * updated docker
    
    * update: files
    
    * start circleci
    
    * start circleci
    
    * updated app.js
    
    * updated app.js
    
    * Bump loader-utils from 2.0.2 to 2.0.4
    
    Bumps [loader-utils](https://github.com/webpack/loader-utils) from 2.0.2 to 2.0.4.
    - [Release notes](https://github.com/webpack/loader-utils/releases)
    - [Changelog](https://github.com/webpack/loader-utils/blob/v2.0.4/CHANGELOG.md)
    - [Commits](https://github.com/webpack/loader-utils/compare/v2.0.2...v2.0.4)
    
    ---
    updated-dependencies:
    - dependency-name: loader-utils
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update Dockerfile
    
    * ffg
    
    * check garna lago env le kaam garx ki nai
    
    * check garna lago env le kaam garx ki nai
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM node:14
 
 WORKDIR /usr/src/app
-
+# RUN apt-get update 
+# RUN useradd -m rab
+# USER rab
 COPY package*.json ./
 RUN npm install
 COPY . .
-RUN yarn build
+RUN npm run build
 CMD [ "npm" , "start" ]
--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
+function add(numbers) {
+  var result = 0;
+  var parts = numbers.split(',');
+  for (var i = 0; i < parts.length; i++) {
+      var integer = parseInt(parts[i]);
+      if (!isNaN(integer)) {
+          if (integer >= 0) {
+              if (integer <= 1000) {
+                  result += integer;
+              }
+          }
+      }
+  }
+
+  return result;
+}
+
+// const greet = (firstName, lastName, greeting) => {
+//   if (typeof firstName === 'string') {
+//     if (typeof lastName === 'string') {
+//       if (typeof greeting === 'string') {
+//         return `${greeting}, ${firstName}${lastName}`;
+//       }
+//     }
+//   }
+// }
+const greet = (firstName, lastName, greeting) => {
+  if (typeof firstName !== 'string') {
+    throw new Error('first name is required');
+  }
+  if (typeof lastName !== 'string') {
+    throw new Error('last name is required');
+  }
+  if (typeof greeting !== 'string') {
+    throw new Error('greeting is required');
+  }
+  return `${greeting}, ${firstName}${lastName}`;
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> HI.
+          Edit <code>src/App.js</code> hello.
         </p>
         <a
           className="App-link"
